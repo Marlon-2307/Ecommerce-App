843c260dae60a7e64905c9172e80115ca4b45f03
Marlon Colon <marloncolon23@gmail.com>
2025-04-28 12:33:39 -0500

    Update

--- __tests__/Navbar.test.tsx
+++ __tests__/Navbar.test.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';  // Asegúrate de importar jest-dom para usar toBeInTheDocument
-import Navbar from '../app/components/Navbar'; // Ajusta la ruta de tu componente
+import '@testing-library/jest-dom';  
+import Navbar from '../app/components/Navbar'; 
 
 test('Navbar muestra el logo correctamente', () => {
   render(<Navbar />);
-  screen.debug(); // Esto imprimirá el DOM renderizado en la consola
+  screen.debug(); 
   const logoElement = screen.getByText(/Julie Boutique./i);
-  expect(logoElement).toBeInTheDocument(); // Verificar que el texto esté en el documento
+  expect(logoElement).toBeInTheDocument(); 
 });
 
